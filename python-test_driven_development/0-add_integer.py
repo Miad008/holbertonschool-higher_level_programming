50f75d29139270105962e6f55bfa4dc93cf884a4
Miad008 <10694@holbertonstudents.com>
2025-05-18 18:43:52 +0000

    k

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a (int or float): first number
+        b (int or float): second number (default is 98)
+
+    Returns:
+        int: sum of a and b as integers
+
+    Raises:
+        TypeError: if a or b is not int or float
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
