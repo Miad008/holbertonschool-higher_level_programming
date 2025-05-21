2826c870b010b7c5eba6f2c0659d68fdccd5cfb0
Miad008 <10694@holbertonstudents.com>
2025-05-21 14:37:50 +0000

    Define a Square class with private size attribute

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
 
-"""This is class Square that defines a square with private size."""
+"""This module defines a class representing a square."""
 
 
 class Square:
+    """A class that represents a square."""
+
     def __init__(self, size):
+        """Initializes the square with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
