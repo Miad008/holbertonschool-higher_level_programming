7c73e9eca2b0d37db5e047097dace287e2a849a3
Miad008 <10694@holbertonstudents.com>
2025-06-01 07:22:32 +0000

    Add CountedIterator class with iteration count

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,45 +1,20 @@
 #!/usr/bin/env python3
 
-class CountedIterator:
-    """
-    Iterator class that counts the number of iterations.
-    It works by wrapping any iterable and keeping track of how many times
-    the next() method has been called.
-    """
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
 
-    def __init__(self, iterable):
-        """
-        Initialize the iterator with the given iterable.
+    def extend(self, iterable):
+        count = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with [{count}] items.")
 
-        :param iterable: The iterable to be wrapped.
-        """
-        self.iterator = iter(iterable)
-        self.count = 0
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
 
-    def __iter__(self):
-        """
-        Return the iterator object itself.
-        This is required for Python's iterator protocol.
-        """
-        return self
-
-    def __next__(self):
-        """
-        Return the next item from the iterator and increment the count.
-
-        If the iterator is exhausted, raise StopIteration.
-        """
-        try:
-            item = next(self.iterator)
-            self.count += 1
-            return item
-        except StopIteration:
-            raise StopIteration  # Just to be explicit, but this is redundant.
-
-    def get_count(self):
-        """
-        Return the number of iterations so far.
-
-        :return: The count of next() calls.
-        """
-        return self.count
+    def pop(self, index=-1):
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)
