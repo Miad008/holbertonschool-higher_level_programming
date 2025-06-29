e40d05f2b3dcafbd21789a9172a643a89c908fba
Miad008 <10694@holbertonstudents.com>
2025-06-29 16:01:10 +0300

    Create database hbtn_0d_usa and table states

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Create database hbtn_0d_usa if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Create table states if it doesn't exist
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
