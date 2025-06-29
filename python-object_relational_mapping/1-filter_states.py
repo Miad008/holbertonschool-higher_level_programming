11c8f44c471d79fb25b97d96ffdbfde05ae4e1eb
Miad008 <10694@holbertonstudents.com>
2025-06-29 13:52:30 +0000

    Task 1: filter states starting with N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with 'N' from the database.
+Lists all states with a name starting with N
 """
-
-import MySQLdb
 import sys
+import MySQLdb
 
-if __name__ == "__main__":
-    # أخذ معلومات الاتصال من سطر الأوامر
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # الاتصال بقاعدة البيانات
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=db_name)
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    # تنفيذ الاستعلام
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
+    states = cur.fetchall()
 
-    # طباعة النتائج
-    for row in cur.fetchall():
-        print(row)
-
-    # إغلاق الاتصال
-    cur.close()
-    db.close()
+    for state in states:
+        print(state)
