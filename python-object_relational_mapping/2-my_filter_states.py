c23535ca28099e51f9da9b46f30ae4173aaee34b
Miad008 <10694@holbertonstudents.com>
2025-06-29 13:50:00 +0000

    Task 2: filter states by user input using format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,21 @@
-#!/usr/bin/python3
+ن#!/usr/bin/python3
 """
-Takes an argument and displays all values in the states table
-where name matches the argument (exact match).
+Lists all values in the states tables of a database where name
+matches the argument
 """
-
-import MySQLdb
 import sys
+import MySQLdb
 
-if __name__ == "__main__":
-    # أخذ بيانات الاتصال والاسم المطلوب البحث عنه
-    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-
-    # الاتصال بقاعدة البيانات
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=db_name)
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
     cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
 
-    # ⚠️ استخدمنا format كما هو مطلوب (لكن هذا غير آمن ضد SQL injection)
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
-    cur.execute(query)
-
-    # طباعة النتائج
-    for row in cur.fetchall():
-        print(row)
-
-    # إغلاق الاتصال
-    cur.close()
-    db.close()
+    for state in states:
+        print(state)
