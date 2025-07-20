2f3c18aba9745dbc767e582a38cc3f27df2ccc78
Miad008 <10694@holbertonstudents.com>
2025-07-20 19:56:58 +0300

    Update task_01_jinja.py

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/about')
 def about():
@@ -23,4 +23,4 @@ def items():
         return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+	app.run(debug=True, port=5000)
