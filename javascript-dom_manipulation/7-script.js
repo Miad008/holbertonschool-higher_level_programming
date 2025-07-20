f8c1330fbc7247682e06fa7d5eb7142ea7ff1550
Miad008 <10694@holbertonstudents.com>
2025-07-20 10:29:18 +0000

    Fetch and list Star Wars movie titles in #list_movies

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+    .then(response => response.json())
+    .then(data => {
+      const movies = data.results;
+      const ul = document.getElementById('list_movies');
+      movies.forEach(movie => {
+        const li = document.createElement('li');
+        li.textContent = movie.title;
+        ul.appendChild(li);
+      });
+    });
+});
